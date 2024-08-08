5c94419acaa3b4523285955637f7bdcf61215ed0
Nolan Shaffer <nshaff3r@Nolans-MacBook-Air.local>
2024-08-08 12:17:34 -0400

    file updates

--- script.sh
+++ script.sh
@@ -2,34 +2,34 @@
 
 # Check if a directory is passed as an argument
 if [ -z "$1" ]; then
-  echo "Usage: $0 <directory> [--repaired]"
+  echo "Usage: $0 <directory> [--task]"
   exit 1
 fi
 
-if [ "$2" == "--repaired" ]; then
-  REPAIRED=true
-else
-  REPAIRED=false
-fi
-
 IMAGE_DIR="$1"
+TASK="$2"
 
-OUTPUT_DIR="./processed"
+OUTPUT_DIR="./expanded-training"
 mkdir -p "$OUTPUT_DIR"
 
 # dimensions for padding
 TARGET_WIDTH=3035
 TARGET_HEIGHT=2396
+COLOR_PROFILE="/Library/Application Support/Adobe/Color/Profiles/Recommended/sRGB Color Space Profile.icm"
 
 # process each image in the directory
 for IMAGE in "$IMAGE_DIR"/*.{jpg,jpeg,png,tif}; do
+
+  # Skip if there are no matching files
+  [ -e "$IMAGE" ] || continue
+
 
   # extract the filename without the path
   FILENAME=$(basename "$IMAGE")
   BASENAME="${FILENAME%.*}"
   TEMP_FILE="$OUTPUT_DIR/temp_$FILENAME"
 
-  if [ $REPAIRED = true ]; then 
+  if [ "$TASK" = "--repaired" ]; then 
 
     # # If in portrait, rotate counter clockwise 90 degrees
     mogrify -rotate "-90<" $IMAGE
@@ -40,11 +40,22 @@ for IMAGE in "$IMAGE_DIR"/*.{jpg,jpeg,png,tif}; do
 
     IMAGE_SIZE=$(identify -format "%wx%h\n" "$OUTPUT_DIR/$FILENAME")
     
+  elif [ "$TASK" = "--resize" ]; then 
+    # Crop and resize each quadrant
+    magick "$IMAGE" -crop 1518x1518+0+0 -resize 256x256 -strip -profile "$COLOR_PROFILE" "$OUTPUT_DIR/${BASENAME}_1.jpg"
+    magick "$IMAGE" -crop 1518x1518+1517+0 -resize 256x256 -strip -profile "$COLOR_PROFILE" "$OUTPUT_DIR/${BASENAME}_2.jpg"
+    magick "$IMAGE" -crop 1518x1518+0+878 -resize 256x256 -strip -profile "$COLOR_PROFILE" "$OUTPUT_DIR/${BASENAME}_3.jpg"
+    magick "$IMAGE" -crop 1518x1518+1517+878 -resize 256x256 -strip -profile "$COLOR_PROFILE" "$OUTPUT_DIR/${BASENAME}_4.jpg"
+    IMAGE_SIZE=$(identify -format "%wx%h\n" "$OUTPUT_DIR/${BASENAME}_4.jpg")
+    
+  elif [ "$TASK" = "--check" ]; then 
+    magick "$IMAGE" -strip -profile "$COLOR_PROFILE" "$OUTPUT_DIR/$BASENAME.jpg" 2>/dev/null
+
   else
     # invert, increase contrast, greyscale, clip white and black points, flip horizontally
     magick "$IMAGE" -brightness-contrast 0x15 -negate -set colorspace Gray -black-threshold 10% -white-threshold 90% -flop "$TEMP_FILE"
     
-    magick mogrify -resize 3035x2396 $TEMP_FILE
+    magick mogrify -resize -strip 3035x2396 $TEMP_FILE
 
     magick "$IMAGE" -background black -gravity center -extent ${TARGET_WIDTH}x${TARGET_HEIGHT} "$OUTPUT_DIR/$FILENAME"
     
--- sort.py
+++ sort.py
@@ -1,19 +1,43 @@
 import os, shutil
 from random import choice
 
-damaged = []
-for file in os.listdir("testing"):
-    damaged.append(file)
+"""
+Script for organizing files
+"""
 
-# remove photos for testing
-for i in range(27):
-    later = choice(damaged)
-    shutil.move(f"training/{later}", f"testing/{later}")
-    damaged.remove(later)
+images = []
+for file in os.listdir("all-processed"):
+    name = file.split(".")[0]
+    if len(name) > 1:
+      images.append(name)  
+    
+
+for file in os.listdir("not-to-use"):
+    if file.split(".")[0] in images:
+        images.remove(file.split(".")[0])
+        images.remove(file.split(".")[0] + "-repaired")
+
+with open("aa.txt", "r") as file:
+    for line in file.readlines():
+        name = line.rstrip().split("/")[-1].split(".")[0]
+        if name in images:
+            images.remove(name)
+            images.remove(name + "-repaired")
+damaged = []
+for image in images:
+    if "repaired" not in image:
+        damaged.append(image)
 print(len(damaged))
 
-for file in os.listdir("repaired-processed"):
-    name = file.split("-")
-    for item in damaged:
-        if name[0] in item:
-            shutil.copyfile(f"repaired-processed/{file}", f"testing/{file}")
+# remove photos for testing
+for i in range(5):
+    random_sel = choice(damaged)
+    later_pair = random_sel + "-repaired.jpg"
+    later = random_sel + ".jpg"
+    shutil.copyfile(f"all-processed/{later}", f"verification/{later}")
+    shutil.copyfile(f"all-processed/{later_pair}", f"verification/{later_pair}")
+    images.remove(random_sel)
+    images.remove(random_sel + "-repaired")
+
+for image in images:
+    shutil.copyfile(f"all-processed/{image}.jpg", f"training-final/{image}.jpg")
